07507a1b7e31baeeaa86353f933b4c32cb58057a
RaulORios <raul.orios89@outlook.com>
2021-08-04 16:46:37 -0300

    Program.cs atualizado

---
+++ src/Exercicio_5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************");
+            Console.WriteLine("*********     Exercicio 5     *********");
+            Console.WriteLine("***************************************");
+
+            Console.WriteLine("");
+
+            int num1;
+            
+            Console.Write("Digite um número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("***************************************");
+
+            Console.WriteLine($"O antecessor ao número {num1} é o número {num1 - 1} .");
+
+            Console.WriteLine("***************************************");
+
+            Console.WriteLine("");
+
+            Console.ReadKey();
+
+        }
+    }
+}
